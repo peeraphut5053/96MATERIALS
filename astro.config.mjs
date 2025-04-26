3c57f9ce023d8ccde32cc8c7b9864c84d785531a
colepalmer <colepalmer@fmlg.cloud>
2025-04-27 00:37:52 +0700

    product cat and review

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  output: "static"
+  output: "static",
+  site: "https://96materials.com/",
+  base: "/",
+  vite: {
+    buildEnd() {
+      fs.copyFileSync('public/CNAME', 'dist/CNAME');
+    }
+  }
 });
