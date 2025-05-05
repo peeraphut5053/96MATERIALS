76f5a8b4a925bfe83ce09e724836a13c16881986
colepalmer <colepalmer@fmlg.cloud>
2025-05-06 00:36:14 +0700

    add new reviews picture

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
+import sitemap from '@astrojs/sitemap';
 
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   output: "static",
   site: "https://96materials.com/",
+  integrations: [sitemap()],
   base: "/",
   vite: {
     buildEnd() {
